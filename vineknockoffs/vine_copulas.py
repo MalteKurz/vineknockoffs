a8c4435aa9572765a135a947998b8716ed116abb
Malte S. Kurz <malte.s.kurz@gmail.com>
2022-06-30 11:48:26 +0200

    move the vine knockoffs into a seperate file

--- vineknockoffs/vine_copulas.py
+++ vineknockoffs/vine_copulas.py
@@ -1,11 +1,6 @@
 import numpy as np
-from scipy.stats import norm
 
 from .copulas import cop_select, IndepCopula
-from ._utils_gaussian_knockoffs import sdp_solver, ecorr_solver
-from ._utils_vine_copulas import dvine_pcorr
-from .copulas import GaussianCopula
-from ._utils_kde import KDEMultivariateWithInvCdf
 
 
 class DVineCopula:
@@ -32,19 +27,6 @@ class DVineCopula:
         u[:, 0] = w[:, 0]
         a[:, 0] = w[:, 0]
         b[:, 0] = w[:, 0]
-
-        # for i in np.arange(1, self.n_vars):
-        #     a[:, 0] = w[:, i]
-        #     for j in np.arange(0, i-1):
-        #         tree = i-j
-        #         cop = j
-        #         a[:, j+1] = self.copulas[tree][cop].vfun(b[:, j], a[:, j])
-        #     u[:, i] = a[:, i]
-        #     b[:, i] = a[:, i]
-        #     for j in np.arange(i-2, -1, -1):
-        #         tree = i-j
-        #         cop = j
-        #         b[:, j] = self.copulas[tree][cop].hfun(b[:, j], a[:, j+1])
 
         for i in np.arange(2, self.n_vars+1):
             a[:, 0] = w[:, i-1]
@@ -112,82 +94,3 @@ class DVineCopula:
                     b[:, i-1] = xx
 
         return cls(copulas)
-
-
-class VineKnockoffs:
-
-    def __init__(self, dvine=None, marginals=None):
-        if (dvine is not None) & (not isinstance(dvine, DVineCopula)):
-            raise TypeError('dvine must be of DVineCopula type. '
-                            f'{str(dvine)} of type {str(type(dvine))} was passed.')
-        self._dvine = dvine
-        self._marginals = marginals
-
-    def generate(self, x_test):
-        n_obs = x_test.shape[0]
-        n_vars = x_test.shape[1]
-        u_test = np.full_like(x_test, np.nan)
-        for i_var in range(n_vars):
-            u_test[:, i_var] = self._marginals[i_var].cdf(x_test[:, i_var])
-
-        sub_dvine = DVineCopula([self._dvine.copulas[tree][:n_vars-tree-1] for tree in np.arange(0, n_vars-1)])
-        u_pits = sub_dvine.compute_pits(u_test)
-        knockoff_pits = np.random.uniform(size=(n_obs, n_vars))
-
-        u_sim = self._dvine.sim(w=np.hstack((u_pits, knockoff_pits)))
-        u_knockoffs = u_sim[:, n_vars:]
-
-        x_knockoffs = np.full_like(x_test, np.nan)
-        for i_var in range(n_vars):
-            x_knockoffs[:, i_var] = self._marginals[i_var].ppf(u_knockoffs[:, i_var])
-        return x_knockoffs
-
-    def fit_gaussian_copula_knockoffs(self, x_train, algo='sdp'):
-        n_vars = x_train.shape[1]
-        self._marginals = [KDEMultivariateWithInvCdf(x_train[:, i_var], 'c')
-                           for i_var in range(n_vars)]
-        u_train = np.full_like(x_train, np.nan)
-        x_gaussian_train = np.full_like(x_train, np.nan)
-        for i_var in range(n_vars):
-            u_train[:, i_var] = self._marginals[i_var].cdf(x_train[:, i_var])
-            x_gaussian_train[:, i_var] = norm.ppf(u_train[:, i_var])
-
-        corr_mat = np.corrcoef(x_gaussian_train.transpose())
-
-        if algo == 'sdp':
-            s_vec = sdp_solver(corr_mat)
-        else:
-            assert algo == 'ecorr'
-            s_vec = ecorr_solver(corr_mat)
-
-        g_mat = np.vstack((np.hstack((corr_mat, corr_mat - np.diag(s_vec))),
-                           np.hstack((corr_mat - np.diag(s_vec), corr_mat))))
-        pcorrs = dvine_pcorr(g_mat)
-        copulas = [[GaussianCopula(rho) for rho in xx] for xx in pcorrs]
-        self._dvine = DVineCopula(copulas)
-
-        return self
-
-    def fit_gaussian_knockoffs(self, x_train, algo='sdp'):
-        n_vars = x_train.shape[1]
-        mus = np.mean(x_train, axis=0)
-        sigmas = np.std(x_train, axis=0)
-        self._marginals = [norm(loc=mus[i_var], scale=sigmas[i_var])
-                           for i_var in range(n_vars)]
-
-        corr_mat = np.corrcoef(x_train.transpose())
-
-        if algo == 'sdp':
-            s_vec = sdp_solver(corr_mat)
-        else:
-            assert algo == 'ecorr'
-            s_vec = ecorr_solver(corr_mat)
-
-        g_mat = np.vstack((np.hstack((corr_mat, corr_mat - np.diag(s_vec))),
-                           np.hstack((corr_mat - np.diag(s_vec), corr_mat))))
-        pcorrs = dvine_pcorr(g_mat)
-        copulas = [[GaussianCopula(rho) for rho in xx] for xx in pcorrs]
-        self._dvine = DVineCopula(copulas)
-
-        return self
-
---
+++ vineknockoffs/vine_knockoffs.py
@@ -0,0 +1,87 @@
+import numpy as np
+from scipy.stats import norm
+
+from vine_copulas import DVineCopula
+from ._utils_gaussian_knockoffs import sdp_solver, ecorr_solver
+from ._utils_vine_copulas import dvine_pcorr
+from .copulas import GaussianCopula
+from ._utils_kde import KDEMultivariateWithInvCdf
+
+
+class VineKnockoffs:
+
+    def __init__(self, dvine=None, marginals=None):
+        if (dvine is not None) & (not isinstance(dvine, DVineCopula)):
+            raise TypeError('dvine must be of DVineCopula type. '
+                            f'{str(dvine)} of type {str(type(dvine))} was passed.')
+        self._dvine = dvine
+        self._marginals = marginals
+
+    def generate(self, x_test):
+        n_obs = x_test.shape[0]
+        n_vars = x_test.shape[1]
+        u_test = np.full_like(x_test, np.nan)
+        for i_var in range(n_vars):
+            u_test[:, i_var] = self._marginals[i_var].cdf(x_test[:, i_var])
+
+        sub_dvine = DVineCopula([self._dvine.copulas[tree][:n_vars-tree-1] for tree in np.arange(0, n_vars-1)])
+        u_pits = sub_dvine.compute_pits(u_test)
+        knockoff_pits = np.random.uniform(size=(n_obs, n_vars))
+
+        u_sim = self._dvine.sim(w=np.hstack((u_pits, knockoff_pits)))
+        u_knockoffs = u_sim[:, n_vars:]
+
+        x_knockoffs = np.full_like(x_test, np.nan)
+        for i_var in range(n_vars):
+            x_knockoffs[:, i_var] = self._marginals[i_var].ppf(u_knockoffs[:, i_var])
+        return x_knockoffs
+
+    def fit_gaussian_copula_knockoffs(self, x_train, algo='sdp'):
+        # ToDo May add alternative methods for the marginals (like parameteric distributions)
+        n_vars = x_train.shape[1]
+        self._marginals = [KDEMultivariateWithInvCdf(x_train[:, i_var], 'c')
+                           for i_var in range(n_vars)]
+        u_train = np.full_like(x_train, np.nan)
+        x_gaussian_train = np.full_like(x_train, np.nan)
+        for i_var in range(n_vars):
+            u_train[:, i_var] = self._marginals[i_var].cdf(x_train[:, i_var])
+            x_gaussian_train[:, i_var] = norm.ppf(u_train[:, i_var])
+
+        corr_mat = np.corrcoef(x_gaussian_train.transpose())
+
+        if algo == 'sdp':
+            s_vec = sdp_solver(corr_mat)
+        else:
+            assert algo == 'ecorr'
+            s_vec = ecorr_solver(corr_mat)
+
+        g_mat = np.vstack((np.hstack((corr_mat, corr_mat - np.diag(s_vec))),
+                           np.hstack((corr_mat - np.diag(s_vec), corr_mat))))
+        pcorrs = dvine_pcorr(g_mat)
+        copulas = [[GaussianCopula(rho) for rho in xx] for xx in pcorrs]
+        self._dvine = DVineCopula(copulas)
+
+        return self
+
+    def fit_gaussian_knockoffs(self, x_train, algo='sdp'):
+        n_vars = x_train.shape[1]
+        mus = np.mean(x_train, axis=0)
+        sigmas = np.std(x_train, axis=0)
+        self._marginals = [norm(loc=mus[i_var], scale=sigmas[i_var])
+                           for i_var in range(n_vars)]
+
+        corr_mat = np.corrcoef(x_train.transpose())
+
+        if algo == 'sdp':
+            s_vec = sdp_solver(corr_mat)
+        else:
+            assert algo == 'ecorr'
+            s_vec = ecorr_solver(corr_mat)
+
+        g_mat = np.vstack((np.hstack((corr_mat, corr_mat - np.diag(s_vec))),
+                           np.hstack((corr_mat - np.diag(s_vec), corr_mat))))
+        pcorrs = dvine_pcorr(g_mat)
+        copulas = [[GaussianCopula(rho) for rho in xx] for xx in pcorrs]
+        self._dvine = DVineCopula(copulas)
+
+        return self
