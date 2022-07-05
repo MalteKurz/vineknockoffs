dabed92c55542c530ee23c30f0f5acaf33c244cf
Malte S. Kurz <malte.s.kurz@gmail.com>
2022-07-05 15:22:33 +0200

    add some tolerance for the bounds of s

--- vineknockoffs/_utils_gaussian_knockoffs.py
+++ vineknockoffs/_utils_gaussian_knockoffs.py
@@ -3,13 +3,13 @@ import cvxpy as cp
 from scipy.linalg import eigh
 
 
-def sdp_solver(corr_mat, tol=1e-4):
+def sdp_solver(corr_mat, tol_psd=1e-4, tol_s=1e-6):
     n_vars = corr_mat.shape[0]
-    tol_mat = np.diag(np.repeat(tol, n_vars))
+    tol_mat = np.diag(np.repeat(tol_psd, n_vars))
     s = cp.Variable(n_vars)
     obj_fun = cp.Maximize(cp.sum(s))
     constraints = [cp.diag(s) + tol_mat << 2 * corr_mat]
-    constraints += [0 <= s, s <= 1]
+    constraints += [tol_s <= s, s <= 1.-tol_s]
 
     prob = cp.Problem(obj_fun, constraints)
     prob.solve(solver='CVXOPT')
