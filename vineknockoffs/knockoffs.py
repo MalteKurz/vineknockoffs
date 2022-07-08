f56fe9a6e5330df6f4409ac13da8ce269a74aec5
Malte S. Kurz <malte.s.kurz@gmail.com>
2022-07-08 17:33:44 +0200

    add derivative for loss

--- vineknockoffs/knockoffs.py
+++ vineknockoffs/knockoffs.py
@@ -94,6 +94,111 @@ class KockoffsLoss:
             + self.gamma * loss_mmd_total + self.delta_corr * loss_corr
         return loss, loss_moments, loss_sdp_corr, loss_mmd_total, loss_corr
 
+    def deriv(self, x, x_knockoffs, x_knockoffs_deriv, swap_inds=None, sdp_corr=None):
+        if sdp_corr is None:
+            corr_mat = np.corrcoef(x.transpose())
+            sdp_corr = 1. - sdp_solver(corr_mat)
+
+        n_obs = x.shape[0]
+        dim_x = x.shape[1]
+        n_pars = x_knockoffs_deriv.shape[2]
+
+        if swap_inds is None:
+            swap_inds = np.arange(0, dim_x)[bernoulli.rvs(0.5, size=dim_x) == 1]
+
+        # Loss part 1: First and second moments
+        mu_data = x.mean(axis=0)
+        mu_knockoffs = x_knockoffs.mean(axis=0)
+
+        x_dm = x - mu_data
+        x_knockoffs_dm = x_knockoffs - mu_knockoffs
+
+        sigma_x = x_dm.T @ x_dm / n_obs
+        sigma_x_knockoffs = x_knockoffs_dm.T @ x_knockoffs_dm / n_obs
+        sigma_x_x_knockoffs = x_knockoffs_dm.T @ x_dm / n_obs
+
+        denom = np.power(sigma_x, 2).sum()
+
+        m_mat = np.ones_like(sigma_x)
+        np.fill_diagonal(m_mat, 0.)
+
+        # Loss part 2: Correlations
+        var_x = sigma_x.diagonal()
+        var_x_knockoffs = sigma_x_knockoffs.diagonal()
+        cov_x_x_knockoffs = sigma_x_x_knockoffs.diagonal()
+        corr_x_x_knockoffs = cov_x_x_knockoffs / np.sqrt(var_x * var_x_knockoffs)
+
+        # Derivatives for loss part 1 & 2
+        loss_moments_deriv = np.zeros(n_pars)
+        loss_sdp_corr_deriv = np.zeros(n_pars)
+        loss_corr_deriv = np.zeros(n_pars)
+        for i_par in range(n_pars):
+            mu_derivs = x_knockoffs_deriv[:, :, i_par].mean(axis=0)
+            x_knockoffs_deriv_dm = x_knockoffs_deriv[:, :, i_par] - mu_derivs
+
+            sigma_x_knockoffs_deriv = x_knockoffs_deriv_dm.T @ x_knockoffs_dm / n_obs \
+                + x_knockoffs_dm.T @ x_knockoffs_deriv_dm / n_obs
+            sigma_x_x_knockoffs_deriv = x_knockoffs_deriv_dm.T @ x_dm / n_obs
+
+            loss_1m_deriv = -2 * ((mu_data - mu_knockoffs) * mu_derivs).sum()
+
+            loss_2m_diag_deriv = -2 * ((sigma_x - sigma_x_knockoffs) * sigma_x_knockoffs_deriv).sum()
+            loss_2m_offdiag_deriv = -2 * (m_mat * ((sigma_x - sigma_x_x_knockoffs) * sigma_x_x_knockoffs_deriv)).sum()
+
+            loss_moments_deriv[i_par] = loss_1m_deriv / dim_x + loss_2m_diag_deriv / denom \
+                + loss_2m_offdiag_deriv / denom
+
+            var_x_knockoffs_deriv = sigma_x_knockoffs_deriv.diagonal()
+            cov_x_x_knockoffs_deriv = sigma_x_x_knockoffs_deriv.diagonal()
+            corr_x_x_knockoffs_deriv = (cov_x_x_knockoffs_deriv * var_x * var_x_knockoffs
+                                        - 0.5 * cov_x_x_knockoffs * var_x_knockoffs_deriv * var_x) \
+                / np.power(var_x * var_x_knockoffs, 1.5)
+
+            loss_sdp_corr_deriv[i_par] = -2 * ((sdp_corr - corr_x_x_knockoffs) * corr_x_x_knockoffs_deriv).sum() / dim_x
+            loss_corr_deriv[i_par] = 2 * (corr_x_x_knockoffs * corr_x_x_knockoffs_deriv).sum() / dim_x
+
+        alphas = np.array([1., 2., 4., 8., 16., 32., 64., 128.])
+        n_obs_half = int(np.floor(n_obs / 2.))
+        x_part1 = x[:n_obs_half, :]
+        x_part2 = x[n_obs_half:, :]
+        x_knockoffs_part1 = x_knockoffs[:n_obs_half, :]
+        x_knockoffs_part2 = x_knockoffs[n_obs_half:, :]
+
+        z1 = np.hstack((x_part1, x_knockoffs_part1))
+        # full swap
+        z2 = np.hstack((x_knockoffs_part2, x_part2))
+
+        # partial swap
+        z3 = np.hstack((x_part2, x_knockoffs_part2))
+        z3[:, swap_inds] = x_knockoffs_part2[:, swap_inds]
+        z3[:, swap_inds + dim_x] = x_part2[:, swap_inds]
+
+        x_deriv_part1 = np.zeros_like(x_part1)
+        x_deriv_part2 = np.zeros_like(x_part2)
+
+        z1_deriv = np.full((n_obs_half, 2*dim_x, n_pars), np.nan)
+        z2_deriv = np.full((n_obs - n_obs_half, 2*dim_x, n_pars), np.nan)
+        z3_deriv = np.full_like(z2_deriv, np.nan)
+
+        for i_par in range(n_pars):
+            x_knockoffs_deriv_part1 = x_knockoffs_deriv[:n_obs_half, :, i_par]
+            x_knockoffs_deriv_part2 = x_knockoffs_deriv[n_obs_half:, :, i_par]
+
+            z1_deriv[:, :, i_par] = np.hstack((x_deriv_part1, x_knockoffs_deriv_part1))
+            z2_deriv[:, :, i_par] = np.hstack((x_knockoffs_deriv_part2, x_deriv_part2))
+            z3_deriv[:, :, i_par] = np.hstack((x_deriv_part2, x_knockoffs_deriv_part2))
+            z3_deriv[:, swap_inds, i_par] = x_knockoffs_deriv_part2[:, swap_inds]
+            z3_deriv[:, swap_inds+dim_x, i_par] = x_deriv_part2[:, swap_inds]
+
+        loss_mmd_full_deriv = self._loss_mmd_deriv(z1, z2, z1_deriv, z2_deriv, alphas)
+        loss_mmd_partial_deriv = self._loss_mmd_deriv(z1, z3, z1_deriv, z3_deriv, alphas)
+
+        loss_mmd_total_deriv = loss_mmd_full_deriv + loss_mmd_partial_deriv
+
+        loss_deriv = self.alpha * loss_moments_deriv + self.delta_sdp_corr * loss_sdp_corr_deriv \
+            + self.gamma * loss_mmd_total_deriv + self.delta_corr * loss_corr_deriv
+        return loss_deriv, loss_moments_deriv, loss_sdp_corr_deriv, loss_mmd_total_deriv, loss_corr_deriv
+
     @staticmethod
     def _loss_mmd(x, y, alphas):
         n = x.shape[0]
@@ -113,3 +218,41 @@ class KockoffsLoss:
         # sqrt loss
         loss = np.sqrt(loss)
         return loss
+
+    @staticmethod
+    def _loss_mmd_deriv(x, y, x_deriv, y_deriv, alphas):
+        n = x.shape[0]
+        m = y.shape[0]
+        n_pars = x_deriv.shape[2]
+
+        z = np.vstack((x, y))
+        zzt = z @ z.T
+        diag_zzt = zzt.diagonal()
+        diag_zzt_mat = np.tile(diag_zzt, (n + m, 1))
+
+        exponent = diag_zzt_mat + diag_zzt_mat.T - 2 * zzt
+        loss = 0
+        loss_vals_for_deriv = np.zeros_like(exponent)
+        for alpha in alphas:
+            loss_vals = np.exp(-exponent / (2 * alpha ** 2))
+            loss_vals_for_deriv += loss_vals * (-1/(2 * alpha ** 2))
+            loss += loss_vals[:n, :n].mean() + loss_vals[n:, n:].mean() - \
+                loss_vals[:n, n:].mean() - loss_vals[n:, 1:n].mean()
+        loss = np.sqrt(loss)
+
+        loss_deriv = np.zeros(n_pars)
+        for i_par in range(n_pars):
+            z_deriv = np.vstack((x_deriv[:, :, i_par], y_deriv[:, :, i_par]))
+            zz_deriv = z @ z_deriv.T
+            diag_zz_deriv = zz_deriv.diagonal()
+            diag_zz_deriv_mat = np.tile(diag_zz_deriv, (n + m, 1))
+
+            exponent_deriv = (2 * diag_zz_deriv_mat + 2 * diag_zz_deriv_mat.T
+                              - 2 * zz_deriv - 2 * zz_deriv.T)
+
+            loss_vals_deriv = exponent_deriv * loss_vals_for_deriv
+            loss_deriv[i_par] = loss_vals_deriv[:n, :n].mean() + loss_vals_deriv[n:, n:].mean() - \
+                loss_vals_deriv[:n, n:].mean() - loss_vals_deriv[n:, 1:n].mean()
+
+        loss_deriv = loss_deriv / (2 * loss)
+        return loss_deriv
--- vineknockoffs/tests/test_vine_knockoffs_funcs.py
+++ vineknockoffs/tests/test_vine_knockoffs_funcs.py
@@ -3,13 +3,16 @@ import pytest
 
 from scipy.stats import norm
 from scipy.linalg import toeplitz
-from scipy.stats import multivariate_normal
+from scipy.stats import multivariate_normal, bernoulli
 
 from statsmodels.tools.numdiff import approx_fprime
 
 from vineknockoffs.copulas import ClaytonCopula, FrankCopula, GumbelCopula, GaussianCopula, IndepCopula
 from vineknockoffs.vine_copulas import DVineCopula
 from vineknockoffs.vine_knockoffs import VineKnockoffs
+from vineknockoffs.knockoffs import KockoffsLoss
+
+from vineknockoffs._utils_gaussian_knockoffs import sdp_solver
 
 np.random.seed(1111)
 
@@ -42,8 +45,15 @@ def test_generate_numdiff(which_par):
     x_data = multivariate_normal(mean=np.zeros(n_vars), cov=cov_mat).rvs(n_obs)
 
     vine_ko = VineKnockoffs()
-    vine_ko.fit_vine_copula_knockoffs(x_data)
-    # vine_ko.fit_gaussian_knockoffs(x_data)
+    # vine_ko.fit_vine_copula_knockoffs(x_data)
+    vine_ko.fit_gaussian_knockoffs(x_data)
+    for t in vine_ko._dvine.copulas:
+        for c in t:
+            if isinstance(c, GaussianCopula):
+                if c.par > 0.99:
+                    c._par = 0.99
+                elif c.par < -0.99:
+                    c._par = -0.99
 
     # u_test = dvine.sim(n_obs)
     # x_test = norm.ppf(u_test)
@@ -75,6 +85,68 @@ def test_generate_numdiff(which_par):
                                                 'ko_eps': knockoff_eps},
                                         centered=True),
                           0, 1)
+
+    assert np.allclose(res_num.astype('float64'),
+                       res,
+                       rtol=1e-4, atol=1e-3)
+
+
+def test_loss_numdiff(which_par):
+    n_obs = 71
+    n_vars = 4
+
+    cov_mat = toeplitz([np.power(0.7, k) for k in range(n_vars)])
+    x_data = multivariate_normal(mean=np.zeros(n_vars), cov=cov_mat).rvs(n_obs)
+
+    vine_ko = VineKnockoffs()
+    # vine_ko.fit_vine_copula_knockoffs(x_data)
+    vine_ko.fit_gaussian_knockoffs(x_data)
+    for t in vine_ko._dvine.copulas:
+        for c in t:
+            if isinstance(c, GaussianCopula):
+                if c.par > 0.99:
+                    c._par = 0.99
+                elif c.par < -0.99:
+                    c._par = -0.99
+
+    x_test = multivariate_normal(mean=np.zeros(n_vars), cov=cov_mat).rvs(n_obs)
+    knockoff_eps = np.random.uniform(size=(n_obs, n_vars))
+
+    x_knockoffs = vine_ko.generate(x_test=x_test, knockoff_eps=knockoff_eps)
+    x_knockoffs_deriv = vine_ko.generate_par_jacobian(x_test=x_test, knockoff_eps=knockoff_eps, which_par=which_par)
+
+    loss = KockoffsLoss(delta_corr=1.)
+    swap_inds = np.arange(0, n_vars)[bernoulli.rvs(0.5, size=n_vars) == 1]
+    corr_mat = np.corrcoef(x_test.transpose())
+    sdp_corr = 1. - sdp_solver(corr_mat)
+    res = loss.deriv(x=x_test, x_knockoffs=x_knockoffs, x_knockoffs_deriv=x_knockoffs_deriv,
+                     swap_inds=swap_inds, sdp_corr=sdp_corr)[0]
+
+    if which_par == 'upper only':
+        start_tree = n_vars
+    else:
+        start_tree = 1
+    par_vec = np.array([cop.par for tree in vine_ko._dvine.copulas[start_tree-1:] for cop in tree if cop.par is not None])
+
+    def generate_for_numdiff(pars, from_tree, xx_test, ko_eps):
+        ind_par = 0
+        for tree in np.arange(from_tree, vine_ko._dvine.n_vars):
+            for cop in np.arange(1, vine_ko._dvine.n_vars-tree+1):
+                if not isinstance(vine_ko._dvine.copulas[tree-1][cop-1], IndepCopula):
+                    vine_ko._dvine.copulas[tree-1][cop-1]._par = pars[ind_par]
+                    ind_par += 1
+        xx_knockoffs = vine_ko.generate(x_test=xx_test, knockoff_eps=ko_eps)
+        xx_res = loss.eval(x=x_test, x_knockoffs=xx_knockoffs,
+                           swap_inds=swap_inds, sdp_corr=sdp_corr)
+        return xx_res[0]
+
+    res_num = approx_fprime(par_vec,
+                            generate_for_numdiff,
+                            epsilon=1e-6,
+                            kwargs={'from_tree': start_tree,
+                                    'xx_test': x_test,
+                                    'ko_eps': knockoff_eps},
+                            centered=True)
 
     assert np.allclose(res_num.astype('float64'),
                        res,
